36a1e0b67a5517a9e30f3e3bcb8b3cd347065495
João Guilherme Cavalcanti <joaogui76@gmail.com>
2024-04-08 14:13:43 -0300

    Melhora a função de adicionar produtos ao carrinho.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let valorTotal;
+let carrinho = {}; // Objeto para armazenar os produtos no carrinho
 
 limpar()
 
@@ -16,22 +17,36 @@ function adicionar() {
         return;
     }
 
-    // Exibe o produto adicionado no carrinho
-    let listaProdutos = document.getElementById('lista-produtos');
-    let produto = document.createElement('section');
-    produto.classList.add('carrinho__produtos__produto');
-    produto.innerHTML = `<span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${valorProduto}</span>`;
-    listaProdutos.appendChild(produto);
-    
+    // Verifica se o produto já está no carrinho
+    if (carrinho.hasOwnProperty(nomeProduto)) {
+        carrinho[nomeProduto].quantidade += quantidade; // Incrementa a quantidade do produto existente no carrinho
+    } else {
+        carrinho[nomeProduto] = {quantidade: quantidade, valor: valorProduto}; // Adiciona um novo produto ao carrinho
+    }
+
+    exibirProdutosNoCarrinho();
+
     valorTotal += (valorProduto * quantidade);
     // Atualiza o valor do carrinho de compras
     document.getElementById('valor-total').textContent = `R$${valorTotal}`;
-
     document.getElementById('quantidade').value = 0
+}
+
+function exibirProdutosNoCarrinho() {
+    let listaProdutos = document.getElementById('lista-produtos');
+    listaProdutos.innerHTML = '';
+
+    for (const produto in carrinho) {
+        let produtoHTML = document.createElement('section');
+        produtoHTML.classList.add('carrinho__produtos__produto');
+        produtoHTML.innerHTML = `<span class="texto-azul">${carrinho[produto].quantidade}x</span> ${produto} <span class="texto-azul">R$${carrinho[produto].valor}</span>`;
+        listaProdutos.appendChild(produtoHTML);
+    }
 }
 
 function limpar() {
     valorTotal = 0;         // Reseta o valor total para 0
+    carrinho = {};          // Limpa o carrinho
 
     document.getElementById('valor-total').textContent = `R$${valorTotal}`;        // Atualiza o valor do carrinho de compras
 
